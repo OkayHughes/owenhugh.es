efd410592142d3de0139058ae079f30044505cf5
Owen Hughes <owhughes@0587456905.wireless.umich.net>
2023-05-10 14:41:00 -0400

    Add mobile site version

--- script.js
+++ script.js
@@ -11,15 +11,30 @@ console.log("Hello 🌎");
 Make the "Click me!" button move when the visitor clicks it:
 - First add the button to the page by following the "Next steps" in the README
 */
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
+// const btn = document.querySelector("button"); // Get the button from the page
+// // Detect clicks on the button
+// if (btn) {
+//   btn.onclick = function() {
+//     // The JS works in conjunction with the 'dipped' code in style.css
+//     btn.classList.toggle("dipped");
+//   };
+// }
 
+
+(function() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const ratio = (width / height);
+  if (ratio > 1){
+    const elem = document.getElementById("style")
+    elem.setAttribute("rel", "stylesheet");
+  } else {
+    const elem = document.getElementById("mobile_style");
+    elem.setAttribute("rel", "stylesheet");
+  }
+  
+
+})();
 // This is a single line JS comment
 /*
 This is a comment that can span multiple lines 
